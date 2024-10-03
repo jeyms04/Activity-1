669df0af9dd22e14e8bb88aef4893a44c0eeceb5
jeyms04 <chustienjames04@gmail.com>
2024-10-03 12:03:27 +0800

    Activity 1

---
+++ task 1.py
@@ -0,0 +1,8 @@
+list_of_fruits = ["mango","apple","banana"]
+num=int(input("how many fruits would you like to insert:"))
+fruits = []
+
+for data in range(num):
+    names = input("enter name of fruits:")
+fruits.append(names)
+print(fruits)
---
+++ task 2.py
@@ -0,0 +1,8 @@
+list_of_fruits = ["banana","apple","orange","mango"]
+
+for data in list_of_fruits:
+if data=="banana":
+    print("hellow banana")
+else:
+    print("apple")
+   
